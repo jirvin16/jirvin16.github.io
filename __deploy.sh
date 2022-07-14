f8625293d70b1f3ae2ab856e597fe9c24cff484b
Jeremy Irvin <jirvin16@stanford.edu>
2022-07-13 18:54:11 -0700

    New pubs and minor changes.

--- __deploy.sh
+++ __deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+ 
+jekyll serve
 
-rm -rf public_html/
+# I don't think this is necessary
+# rm -rf public_html/
 
 # Upload to server
-scp -r _site/* public_html/
+# scp -r _site/* public_html/
 
